c4d98a27e6b884482c9906551fda2de7311c5f24
hrapov-v <hrapov-v@bk.ru>
2021-07-27 22:34:29 +0300

    Создал Binding

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import superky.keytwo.mykotlinapp.R
+import superky.keytwo.mykotlinapp.databinding.MainFragmentBinding
 import superky.keytwo.mykotlinapp.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
     lateinit var viewModel: MainViewModel
+    var _binding: MainFragmentBinding? = null
+
+
 
     companion object {
         fun newInstance() = MainFragment()
@@ -24,7 +29,10 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        val textView: TextView? = view.findViewById(R.id.test)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
