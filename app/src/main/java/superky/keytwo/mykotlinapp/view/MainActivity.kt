29f596edd631dec8d50f92ae28db220fa992ce55
hrapov-v <hrapov-v@bk.ru>
2021-07-27 21:02:57 +0300

    Почему то выдаёт ошибку

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainActivity.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainActivity.kt
@@ -2,12 +2,18 @@ package superky.keytwo.mykotlinapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import superky.keytwo.mykotlinapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container_one, MainFragment.newInstance()).commit()
+        }
         
     }
 }
+
+
--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
@@ -1,11 +1,23 @@
 package superky.keytwo.mykotlinapp.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import superky.keytwo.mykotlinapp.R
 
 class MainFragment : Fragment() {
-    companion object{
-        fun newInstance() : Fragment {
-            return MainFragment()
-        }
+    companion object {
+        fun newInstance()= MainFragment
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
 }
