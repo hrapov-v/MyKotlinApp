f802cba38e98561685d78038de194c46ef502c4d
hrapov-v <hrapov-v@bk.ru>
2021-07-27 21:45:33 +0300

    Инициализировал в фрагменте

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import superky.keytwo.mykotlinapp.R
+import superky.keytwo.mykotlinapp.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
+
+    lateinit var viewModel: MainViewModel
+
     companion object {
         fun newInstance() = MainFragment()
     }
