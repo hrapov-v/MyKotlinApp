4f4cdab02e0c33fd31e9dd648cfa80252077c728
hrapov-v <hrapov-v@bk.ru>
2021-08-01 18:57:59 +0300

    переопределил onCreateViewHolder() в адаптере

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragmentAdapter.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragmentAdapter.kt
@@ -1,16 +1,22 @@
 package superky.keytwo.mykotlinapp.view
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import superky.keytwo.mykotlinapp.R
 import superky.keytwo.mykotlinapp.model.Weather
 
 class MainFragmentAdapter : RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder> {
 
-    private var weatherData : List<Weather> = listOf()
+    private var weatherData: List<Weather> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        val mainViewHolder : MainViewHolder
+        val view: View =
+            (LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_main_recycler_item, parent, false
+            )) as View
+        return MainViewHolder(view)
     }
 
     class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
