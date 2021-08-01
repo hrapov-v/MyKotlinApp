b1f9f94b2ba792db8572e865d0f7ee0745cdfe47
hrapov-v <hrapov-v@bk.ru>
2021-08-01 19:14:51 +0300

    Закончил адпатер

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragmentAdapter.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainFragmentAdapter.kt
@@ -3,23 +3,41 @@ package superky.keytwo.mykotlinapp.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import superky.keytwo.mykotlinapp.R
 import superky.keytwo.mykotlinapp.model.Weather
 
-class MainFragmentAdapter : RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder> {
+class MainFragmentAdapter : RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder>() {
 
-    private var weatherData: List<Weather> = listOf()
+    fun setWeather(list : List<Weather>) {
+        weatherData = list
+        notifyDataSetChanged()
+    }
+
+    //private var weatherData: List<Weather> = listOf()
+    private lateinit var weatherData: List<Weather>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        val view: View =
+        val view =
             (LayoutInflater.from(parent.context).inflate(
                 R.layout.fragment_main_recycler_item, parent, false
-            )) as View
+            ))
         return MainViewHolder(view)
     }
 
-    class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.init(weatherData[position])
+    }
 
+    override fun getItemCount(): Int {
+        return weatherData.size
+    }
+
+    class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun init(weather: Weather) {
+            itemView.findViewById<TextView>(R.id.mainFragmentRecyclerItemTextView).text =
+                weather.city.city
+        }
     }
 }
