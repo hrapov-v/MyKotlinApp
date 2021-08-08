c085fa5f80de4f639bd8a650647aa64cacafecb3
hrapov-v <hrapov-v@bk.ru>
2021-08-08 11:23:37 +0300

    Ещё сократил с помощью let

--- app/src/main/java/superky/keytwo/mykotlinapp/view/MainActivity.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/MainActivity.kt
@@ -13,11 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if (savedInstanceState == null) {
+        savedInstanceState.let {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance()).commit()
         }
-
     }
 }
 
