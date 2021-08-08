ef94bb75488a2769adc0ecda3f69afc900774370
hrapov-v <hrapov-v@bk.ru>
2021-08-09 00:12:31 +0300

    Всё работает но с моим ключом почему то не работает

--- app/src/main/java/superky/keytwo/mykotlinapp/model/Const.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/model/Const.kt
@@ -2,4 +2,4 @@ package superky.keytwo.mykotlinapp.model
 
 const val YANDEX_API_URL = "https://api.weather.yandex.ru/v2/informers"
 const val YANDEX_API_KEY_NAME = "X-Yandex-API-Key"
-const val YANDEX_API_KEY_VALUE = "060e3a1b-a61b-41b4-99bd-77f999c69547" //"ceae3d76-b634-4bfd-8ef5-25a327758ae9"
+const val YANDEX_API_KEY_VALUE = "060e3a1b-a61b-41b4-99bd-77f999c69547" //"ceae3d76-b634-4bfd-8ef5-25a327758ae9" //С моим ключом почему то не работает
