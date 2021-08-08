77eac4cb232f52dc3f971fb27bdbf52bf361f407
hrapov-v <hrapov-v@bk.ru>
2021-08-08 22:36:31 +0300

    Сделал лоадер

--- app/src/main/java/superky/keytwo/mykotlinapp/model/Const.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/model/Const.kt
@@ -1,3 +1,5 @@
 package superky.keytwo.mykotlinapp.model
 
 const val YANDEX_API_URL = "https://api.weather.yandex.ru/v2/informers"
+const val YANDEX_API_KEY_NAME = "X-Yandex-API-Key"
+const val YANDEX_API_KEY_VALUE = "060e3a1b-a61b-41b4-99bd-77f999c69547"
--- app/src/main/java/superky/keytwo/mykotlinapp/view/details/WeatherLoader.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/details/WeatherLoader.kt
@@ -1,8 +1,16 @@
 package superky.keytwo.mykotlinapp.view.details
 
+import com.google.gson.Gson
 import superky.keytwo.mykotlinapp.model.WeatherDTO
+import superky.keytwo.mykotlinapp.model.YANDEX_API_KEY_NAME
+import superky.keytwo.mykotlinapp.model.YANDEX_API_KEY_VALUE
 import superky.keytwo.mykotlinapp.model.YANDEX_API_URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
 import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
 class WeatherLoader(
     private val listener: WeatherLoaderListener,
@@ -10,7 +18,21 @@ class WeatherLoader(
     private val lon: Double
 ) {
     fun loadWeather() {
-        val url = URL("${YANDEX_API_URL}?lat= ${lat}& ")
+        Thread {
+            try {
+                val url = URL("${YANDEX_API_URL}?lat= ${lat}&lon= ${lon}")
+                val httpsURLConnection: HttpsURLConnection =
+                    url.openConnection() as HttpsURLConnection
+                httpsURLConnection.connectTimeout = 5000
+                httpsURLConnection.requestMethod = "GET"
+                httpsURLConnection.addRequestProperty(YANDEX_API_KEY_NAME, YANDEX_API_KEY_VALUE)
+                val buffer = BufferedReader(InputStreamReader(httpsURLConnection.inputStream))
+                val weatherDTO: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
+                listener.onLoaded(weatherDTO)
+            } catch (e: Exception) {
+                listener.onFailed(e)
+            }
+        }
     }
 }
 
