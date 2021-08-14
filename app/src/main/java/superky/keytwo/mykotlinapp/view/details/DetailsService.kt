eab5127bba4e1470a66edd8795e58f80ddc4bab7
hrapov-v <hrapov-v@bk.ru>
2021-08-14 20:37:35 +0300

    Пенес константы и метод загрузки погоды

--- app/src/main/java/superky/keytwo/mykotlinapp/view/details/DetailsService.kt
+++ app/src/main/java/superky/keytwo/mykotlinapp/view/details/DetailsService.kt
@@ -1,5 +1,70 @@
 package superky.keytwo.mykotlinapp.view.details
 
-class DetailsService {
+import android.app.IntentService
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Handler
+import com.google.gson.Gson
+import superky.keytwo.mykotlinapp.model.WeatherDTO
+import superky.keytwo.mykotlinapp.model.YANDEX_API_KEY_NAME
+import superky.keytwo.mykotlinapp.model.YANDEX_API_KEY_VALUE
+import superky.keytwo.mykotlinapp.model.YANDEX_API_URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+const val DETAILS_INTENT_FILTER = "DETAILS_INTENT_FILTER"
+const val DETAILS_LOAD_RESULT_EXTRA = "LOAD RESULT"
+const val DETAILS_INTENT_EMPTY_EXTRA = "INTENTS IS EMPTY"
+const val DETAILS_DATA_EMPTY_EXTRA = "DATA IS EMPTY"
+const val DETAILS_RESPONSE_EMPTY_EXTRA = "RESPONSE IS EMPTY"
+const val DETAILS_REQUEST_ERROR_EXTRA = "REQUEST ERROR"
+const val DETAILS_REQUEST_ERROR_MESSAGE_EXTRA = "REQUEST ERROR MESSAGE"
+const val DETAILS_URL_MALFORMED_EXTRA = "URL MALFORMED"
+const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
+const val DETAILS_TEMP_EXTRA = "TEMPERATURE"
+const val DETAILS_FEELS_LIKE_EXTRA = "FEELS LIKE"
+const val DETAILS_CONDITION_EXTRA = "CONDITION"
+
+const val LATITUDE_EXTRA = "Latitude"
+const val LONGITUDE_EXTRA = "Longitude"
+private val REQUEST_GET = "GET"
+private val REQUEST_TIMEOUT = 10000
+private val REQUEST_API_KEY = "X-Yandex-API-Key"
+
+class DetailsService(name: String = "name") : IntentService(name) {
+
+    private val broadcastIntent = Intent(DETAILS_INTENT_FILTER)
+
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+
+    private fun loadWeather(lat: Double, lon: Double) {
+        val handler = Handler()
+        Thread {
+
+            try {
+                val url = URL("$YANDEX_API_URL?lat=${lat}&lon=${lon}")
+                val httpsURLConnection: HttpsURLConnection =
+                    url.openConnection() as HttpsURLConnection
+                httpsURLConnection.connectTimeout = 5000
+                httpsURLConnection.requestMethod = "GET"
+                httpsURLConnection.addRequestProperty(YANDEX_API_KEY_NAME, YANDEX_API_KEY_VALUE)
+                //httpsURLConnection.addRequestProperty(YANDEX_API_KEY_NAME, BuildConfig.YANDEX_API_KEY_NAME)
+                val buffer = BufferedReader(InputStreamReader(httpsURLConnection.inputStream))
+                val weatherDTO: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
+                handler.post(Runnable { listener.onLoaded(weatherDTO) })
+            } catch (e: Exception) {
+                handler.post(Runnable { listener.onFailed(e) })
+            }
+
+        }.start()
+    }
+
+    fun putLoadResult(result: String){
+        broadcastIntent.putExtra(DETAILS_LOAD_RESULT_EXTRA, result)
+    }
 
 }
